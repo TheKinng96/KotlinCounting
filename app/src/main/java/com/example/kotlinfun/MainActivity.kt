315e997f7a511dfccf31c7760404df266af90511
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-18 21:04:07 +0900

    first commit

---
+++ app/src/main/java/com/example/kotlinfun/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinfun
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var textView = findViewById<TextView>(R.id.textView);
+        textView.setText("Count")
+    }
+}
